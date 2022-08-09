3b852b77f7040747291dac98bf8eddfc832c021a
Jeffrey Tom <jct684@gmail.com>
2022-08-09 00:38:20 -0700

    realized flag unnecessary for inside out approach, also improved to O(1) space complexity

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -2,28 +2,20 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         #inside out approach for determining palindrome
         #time complexity O(n^2), O(n) to iterate through list, O(n) to determine outward expansion
-        #space complexity O(k) where k is length of substring
-        #to improve space complexity, we could probably just return the left and right ends and then return the sliced string s
-        highest_len = 0
-        substring_saved = s[0]
+        #space complexity O(1)
+        self.highest_len = 0
+        self.start = 0
+        self.end = 1
         for i in range(len(s)-1):
-            substring_1 = self.expand_outward(s, i, i)
-            substring_2 = self.expand_outward(s, i, i+1)
-            if len(substring_1) > highest_len:
-                highest_len = len(substring_1)
-                substring_saved = substring_1
-            if len(substring_2) > highest_len:
-                highest_len = len(substring_2)
-                substring_saved = substring_2
-        return substring_saved
+            self.expand_outward(s, i, i)
+            self.expand_outward(s, i, i+1)
+        return s[self.start:self.end]
             
     def expand_outward(self, s, left, right):
-        substring = s[left]
-        expand = False
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
-            expand = True
-        if expand:
-            substring = s[left+1:right]
-        return substring
+        if self.highest_len < right - left + 1:
+            self.highest_len = right - left + 1
+            self.start = left+1
+            self.end = right
