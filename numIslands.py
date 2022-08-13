780d1270ca800ce67bb91d70c80b3650c3515774
Jeffrey Tom <jct684@gmail.com>
2022-08-13 02:03:54 -0700

    different, slower approach using a dctionary that doesn't modify input

--- numIslands.py
+++ numIslands.py
@@ -4,17 +4,20 @@ class Solution:
         #time complexity O(row*column)
         #space complexity O(number of lands)
         num_islands = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == "1":
+        visited = {}
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if visited.get((row, col), False):
+                    continue
+                if grid[row][col] == "1":
                     num_islands += 1
-                    self.DFS(grid,i,j)
+                    DFS(grid, row, col, visited)
         return num_islands
 
-    def DFS(self, grid, row, col):
-        if not(row > len(grid)-1 or row < 0 or col > len(grid[row])-1 or col < 0 or grid[row][col] == "0"):
-            grid[row][col] = "0"
-            self.DFS(grid, row, col+1)
-            self.DFS(grid, row+1, col)
-            self.DFS(grid, row-1, col)
-            self.DFS(grid, row, col-1)
+def DFS(grid, row, col, visited):
+    if not(row > len(grid)-1 or row < 0 or col > len(grid[row])-1 or col < 0 or grid[row][col] == "0" or visited.get(tuple([row, col]))):
+        visited[(row,col)] = True
+        DFS(grid, row, col+1, visited)
+        DFS(grid, row+1, col, visited)
+        DFS(grid, row-1, col, visited)
+        DFS(grid, row, col-1, visited)
