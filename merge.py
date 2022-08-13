f65da3d1cf20f83b2acaab07b848dc97a47e6552
Jeffrey Tom <jct684@gmail.com>
2022-08-12 23:22:53 -0700

    python file type

---
+++ merge.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        #sort based on start of interval
+        #if the start is before the previous end then extend the interval to the new end
+        #otherwise create a new interval
+        #O(n*log(n) + n) = O(n log(n)) time complexity
+        #space complexity O(log n) to sort
+        intervals.sort(key=lambda x: x[0])
+        ans = [[intervals[0][0], intervals[0][1]]]
+        j=0
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= ans[j][1]:
+                ans[j][1] = max(ans[j][1], intervals[i][1])
+            else:
+                print(intervals[i][0], intervals[i-1][1])
+                ans.append([intervals[i][0], intervals[i][1]])
+                j += 1
+        return ans
