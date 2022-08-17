3b9dcc942305f79480ca2c1d29f01ec9d65eb9b0
Jeffrey Tom <jct684@gmail.com>
2022-08-16 22:44:35 -0700

    different solution, it's interesting to use heap

--- minMeetingRooms.py
+++ minMeetingRooms.py
@@ -1,23 +1,19 @@
+import heapq
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        return min_meeting_rooms(intervals)       
-        #sort by starting time and ending time independently
-        #start time is room +1, end time is room -1
-        #return max number of concurrent rooms
-        #I think is the same O(n log n ) time complexity, space complexity at O(n)
-
+        return min_meeting_rooms(intervals)
+        #sort based on start times
+        #store end time in a min heap
+        #use min heap to determine if a meeting room is open, otherwise add to heap
+        #size of heap gives us the number of required rooms
+        
 def min_meeting_rooms(intervals):
-    start = [interval[0] for interval in intervals]
-    end = [interval[1] for interval in intervals]
-    start.sort()
-    end.sort()
-    start_p, end_p, max_rooms, rooms = 0, 0, 0, 0
-    while start_p  < len(start):
-        if start[start_p]  < end[end_p]:
-            rooms += 1
-            start_p += 1
+    sorted_intervals = sorted(intervals, key= lambda x:x[0])
+    heap_m = [sorted_intervals[0][1]]
+    for i in range(1, len(sorted_intervals)):
+        if sorted_intervals[i][0] < heap_m[0]:
+            heapq.heappush(heap_m, sorted_intervals[i][1])
         else:
-            rooms -= 1
-            end_p += 1
-        max_rooms = max(max_rooms, rooms)
-    return max_rooms
+            heapq.heapreplace(heap_m, sorted_intervals[i][1])
+    return len(heap_m)
