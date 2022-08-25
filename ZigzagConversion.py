7483d2e320f6d612da1dd619ee7771f67c14bcc0
Jeffrey Tom <jct684@gmail.com>
2022-08-25 00:11:46 -0700

    same logic but with a dictionary

--- ZigzagConversion.py
+++ ZigzagConversion.py
@@ -11,12 +11,17 @@ def zigzag_conversion(s, numRows):
     #space complexity O(n)
     row_p = 0
     prev_p = -1
-    all_rows = [[] for _ in range(numRows)]
+    rows = {}
     final_string = ""
     if numRows == 1:
         return s
     for char in s:
-        all_rows[row_p].append(char)
+        if rows.get(row_p):
+            temp = rows.get(row_p)
+            temp.append(char)
+            rows[row_p] = temp
+        else:
+            rows[row_p] = [char]
         if row_p == numRows-1:
             prev_p = row_p
             row_p -= 1
@@ -29,6 +34,6 @@ def zigzag_conversion(s, numRows):
         elif prev_p < row_p:
             prev_p = row_p
             row_p += 1
-    for row in all_rows:
-        final_string += "".join(row)
+    for key in rows:
+        final_string += "".join(rows.get(key))
     return final_string
