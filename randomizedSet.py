adc2237a6cc0677605c1a7d2dfc8c088ae0b75e0
Jeffrey Tom <jct684@gmail.com>
2022-09-02 09:28:47 -0700

    cleaned things up

--- randomizedSet.py
+++ randomizedSet.py
@@ -5,31 +5,26 @@ class RandomizedSet:
     def __init__(self):
         self.dict = {}
         self.index = []
-        self.size = 0
 
     def insert(self, val: int) -> bool:
         if val in self.dict:
             return False
+        self.dict[val] = len(self.index)
         self.index.append(val)
-        self.dict[val] = self.size
-        self.size += 1
         return True
 
     def remove(self, val: int) -> bool:
         if val in self.dict:
-            if len(self.index) > 1 and self.dict.get(val) != self.size - 1:
-                temp = self.index.pop()
-                self.index[self.dict.get(val)] = temp
-                self.dict[temp] = self.dict.get(val)
-            else:
-                self.index.pop()
+            if len(self.index) > 1: #this isn't necessary but maybe speeds things up a small bit
+                self.dict[self.index[-1]] = self.dict[val]
+                self.index[self.dict[val]], self.index[-1] = self.index[-1], self.index[self.dict[val]]
+            self.index.pop()
             del self.dict[val]
-            self.size -= 1
             return True
         return False
         
     def getRandom(self) -> int:
-        random_num = random.randint(0, self.size - 1)
+        random_num = random.randint(0, len(self.index) - 1)
         return self.index[random_num]
 
 # Your RandomizedSet object will be instantiated and called as such:
