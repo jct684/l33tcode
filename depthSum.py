871e2ccd73fe4de75e08af88db823f41b2262065
Jeffrey Tom <jct684@gmail.com>
2022-09-02 00:08:33 -0700

    python file type

---
+++ depthSum.py
@@ -0,0 +1,63 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        #identify depth for multiplier based on level
+        #identify if isinstance(item, string/int)
+        #time complexity O(n)
+        #space complexity O(N)
+        return recursive_calculate_int_depth(nestedList)
+
+def recursive_calculate_int_depth(array, total=0, depth=0):
+    depth += 1
+    for index in range(len(array)):
+        if array[index].isInteger():
+            total += array[index].getInteger() * depth
+        else:
+            total = recursive_calculate_int_depth(array[index].getList(), total, depth)
+    return total
+        
+    
+                
+        
