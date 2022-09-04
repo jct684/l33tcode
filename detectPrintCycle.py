a75e3e8f4c413d63712dfd873797eaccc4fd1460
Jeffrey Tom <jct684@gmail.com>
2022-09-04 12:31:50 -0700

    while not better worst case complexity, I think this is better solution

--- detectPrintCycle.py
+++ detectPrintCycle.py
@@ -5,27 +5,27 @@
 
 def detect_print_cycle(graph):
     root = 1
-    cycle_membership={}
-    DFS(root, graph, cycle_membership)
-    cycle = [key for key, value in cycle_membership.items() if value == 1]
+    cycle=[]
+    DFS(root, graph, cycle)
     if len(cycle) == 0:
         print("No cycle found")
     else:
         return cycle
 
-def DFS(root, graph, cycle_membership, visited={}, parents={}):
+def DFS(root, graph, cycle, visited={}, parents={}):
     visited[root] = True
     for i in range(len(graph[root])):
-        if graph[root][i] is not parents.get(root, False) and cycle_membership.get("found", True):
+        if graph[root][i] is not parents.get(root, False):
             if graph[root][i] in visited:
-                cycle_membership[graph[root][i]] = 1
-                cycle_membership["found"] = False
-                current = root
-                while current != graph[root][i]:
-                    cycle_membership[current] = 1
-                    current = parents[current]
-                return
-            else:
-                parents[graph[root][i]] = root
-                DFS(graph[root][i], graph, cycle_membership, visited, parents)
-
+                cycle.append(root)
+                return graph[root][i]
+            parents[graph[root][i]] = root
+            found = DFS(graph[root][i], graph, cycle, visited, parents)
+            if found:
+                if found != root:
+                    cycle.append(root)
+                    return found
+                else:
+                    cycle.append(root)
+                    return cycle
+    return ""
