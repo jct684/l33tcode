ffc88791482403245e9ac92c69e709cb74163666
Jeffrey Tom <jct684@gmail.com>
2022-10-17 17:04:21 -0700

    replaced with different maxProfit problem that is harder dp problem

--- maxProfit.py
+++ maxProfit.py
@@ -1,16 +1,17 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        #dynamic programming solution
-        #OPT(i) = maximum profit achieved where i is the first day you can buy
-        #OPT(i) = max(OPT(n-i), profit of selling at current day compared to buying previous days)
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        #OPT(i, buy/sell) = maximum profit by buying/selling at day i
+        #OPT(i, buy) = max(OPT(i-1, buy), OPT(i-1, sell) - prices[i] - fee)
+        #when you are buying, you either held previous buy or sold previous step and are buying again
+        #OPT(i, sell) = max(OPT(i-1, sell), OPT(i-1, buy) + prices[i])
+        #when you are selling, you either bought/held previously or are buying and cannot sell
         #time complexity O(n)
         #space complexity O(n)
-        #OPT(n-1) = 0
-        n = len(prices)
-        memo = [0 for _ in range (n + 1)]
-        memo[n] = 0
-        highest_num = prices[n-1]
-        for i in range (n - 1, 0, -1):
-            memo[i] = max(memo[i+1], highest_num - prices[i-1])
-            highest_num = max(highest_num, prices[i-1])
-        return memo[1]
+        #space complexity could be reduced since we only really need the previous index
+        memo = [[float('-inf'), float('-inf')] for _ in range (len(prices))]
+        memo[0][0] = -prices[0] - fee
+        memo[0][1] = 0
+        for i in range (1, len(memo)):
+            memo[i][0] = max(memo[i-1][0], memo[i-1][1] - prices[i] - fee)
+            memo[i][1] = max(memo[i-1][1], memo[i-1][0] + prices[i])
+        return memo[len(prices)-1][1]
