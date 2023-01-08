be1b2bf0fcf19f073482e8bac7a0a15035dc1fc1
Jeffrey Tom <jct684@gmail.com>
2023-01-08 15:10:42 -0800

    solved again five months later

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,33 +1,46 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = self.head
-        
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = LinkedList()
-        carry = 0
-        while l1 or l2 or carry > 0:
-            new_node = ListNode()
-            l1_val = l1.val if l1 else 0
-            l2_val = l2.val if l2 else 0
-            new_node.val =(l1_val + l2_val + carry) % 10
-            if (l1_val + l2_val + carry) >= 10:
-                carry = int((l1_val + l2_val + carry)/10)
-            else:
-                carry = 0
-            if res.head is None:
-                res.head = new_node
-                res.tail = res.head
-            else:
-                res.tail.next = new_node
-                res.tail = res.tail.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-        return res.head
+        return add_two_numbers(l1, l2)
+
+def add_two_numbers(l1, l2):
+    remainder = 0
+    current_sum = l1.val + l2.val
+    if current_sum > 9:
+        remainder = 1
+        current_sum = current_sum % 10
+    head = ListNode(current_sum)
+    ans = head
+    l1 = l1.next
+    l2 = l2.next
+    while(l1 != None or l2 != None):
+        if l1 == None:
+            current_sum = l2.val + remainder
+            ans, remainder = add_new_node(current_sum, ans)
+            l2 = l2.next
+        elif l2 == None:
+            current_sum = l1.val + remainder
+            ans, remainder = add_new_node(current_sum, ans)
+            l1 = l1.next
+        else:
+            current_sum = l1.val + l2.val + remainder
+            ans, remainder = add_new_node(current_sum, ans)
+            l1 = l1.next
+            l2 = l2.next
+    if remainder == 1:
+        ans.next = ListNode(1)
+    return head
+
+def add_new_node(current_sum, ans):
+    if current_sum > 9:
+        remainder = 1
+        current_sum = current_sum % 10
+        ans.next = ListNode(current_sum)
+    else:
+        remainder = 0
+        ans.next = ListNode(current_sum)
+    return ans.next, remainder
