d03cb1ddd1d45a56816f51c15f4f1a65935e1ee8
Jeffrey Tom <jct684@gmail.com>
2023-02-14 09:32:21 -0800

    bucket sort

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,11 +1,9 @@
-import heapq
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #O(1) time/space complexity
         if k == len(nums):
             return nums
-        #time complexity O(n log m)
+        #time complexity O(n)
         #space complexity O(n)
         freq_dict = {}
         for num in nums:
@@ -13,18 +11,18 @@ class Solution:
                 freq_dict[num] = 1
             else:
                 freq_dict[num] += 1
-        rev_freq_dict = {}
-        heap = []
+        freq_array = [None for _ in range(len(nums)+1)]
         for key, value in freq_dict.items():
-            if value not in rev_freq_dict:
-                rev_freq_dict[value] = [key]
-                heap.append(value)
+            if freq_array[value] == None:
+                freq_array[value] = [key]
             else:
-                rev_freq_dict[value].append(key)
-                heap.append(value)
-        heapq.heapify(heap)
-        k_most_freq = heapq.nlargest(k, heap)
+                freq_array[value].append(key)
+        k_count = 0
         ans = []
-        for value in k_most_freq:
-            ans.append(rev_freq_dict[value].pop())
+        while k_count < k:
+            temp = freq_array.pop()
+            if temp != None:
+                for _ in range(len(temp)):
+                    ans.append(temp.pop())
+                    k_count += 1
         return ans
