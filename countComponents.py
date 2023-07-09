32bb19da93378d63d6abbcd793ea722af37cf300
Jeffrey Tom <jct684@gmail.com>
2023-07-09 15:48:24 -0700

    initial commit

--- countComponents.py
+++ countComponents.py
@@ -1,33 +1,33 @@
-from collections import deque
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         #input: number of nodes and an array
         #output: integer
         # n=4, [[1,2],[2,3],[4,5]] ans = 2
         # n=4, [[1,2],[[4,5], [2,3]], [5,1]] ans = 1
-        #bfs starting at a node that has not been visited before until all nodes visited, count number of fresh starts
         #time complexity O(V+E)
-        #space complexity O(V+E)
-        adj_dict = {}
-        for edge in edges:
-            if edge[0] not in adj_dict:
-                adj_dict[edge[0]] = []
-            if edge[1] not in adj_dict:
-                adj_dict[edge[1]] = []
-            adj_dict[edge[0]].append(edge[1])
-            adj_dict[edge[1]].append(edge[0])
-        visited = set()
-        count = 0
-        q = deque()
-        for node in adj_dict:
-            if node not in visited:
-                q.append(node)
-                count += 1
-                while len(q) > 0:
-                    curr_node = q.popleft()
-                    visited.add(curr_node)
-                    list_nodes = adj_dict[curr_node]
-                    for connected_node in list_nodes:
-                        if connected_node not in visited:
-                            q.append(connected_node)
-        return count + n-len(visited)
+        #space complexity O(V)
+        parents = [i for i in range(n)]
+        size = [1] * n
+        
+        def find(n1):
+            res = n1
+            while res != parents[res]:
+                parents[res] = parents[parents[res]]
+                res = parents[res]
+            return res
+        
+        def combine(n2, n3):
+            p1, p2 = find(n2), find(n3)
+            if p1 == p2:
+                return 0
+            if size[n2] > size[n1]:
+                parents[p2] = p1
+                size[n2] += size[n3]
+            else:
+                parents[p1] = p2
+                size[n3] += size[n2]
+            return 1
+        res = n
+        for n1, n2 in edges:
+            res -= combine(n1,n2)
+        return res
