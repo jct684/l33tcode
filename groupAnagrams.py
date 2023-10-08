4b77b25596b09106d156d49813b8fe90cf73ca20
Jeffrey Tom <jct684@gmail.com>
2023-10-08 10:32:21 -0700

    update

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -3,15 +3,11 @@ class Solution:
         #O(m*n log n) time complexity
         #O(m*n) space complexity
         ana_dict = {}
-        for word in strs:
-            curr_word = sorted(word)
-            curr_word = str(curr_word)
-            if curr_word not in ana_dict:
-                ana_dict[curr_word] = [word]
+        for index in range (len(strs)):
+            word_sort = str(sorted(strs[index]))
+            if word_sort in ana_dict:
+                ana_dict[word_sort].append(strs[index])
             else:
-                ana_dict[curr_word].append(word)
-        ans = []
-        for value in ana_dict.values():
-            ans.append(value)
+                ana_dict[word_sort] = [strs[index]]
+        ans = [val for val in ana_dict.values()]
         return ans
-        
