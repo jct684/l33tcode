ebe116299bc1e72eb7a346929efbabf6d8cd8ebb
Jeffrey Tom <jct684@gmail.com>
2024-08-21 20:27:40 -0700

    update

--- combinationSum.py
+++ combinationSum.py
@@ -1,18 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        total = 0
-        curr_list = []
-        index = 0
-        def dfs(index, curr_list, total):
-            if total == target:
-                res.append(curr_list.copy())
+        
+        def backtrack(candidate, start, total):
+            #output solution
+            if target == total:
+                res.append(candidate[:])
                 return
-            if index == len(candidates) or total > target:
+            if target < total:
                 return
-            curr_list.append(candidates[index])
-            dfs(index, curr_list, total + candidates[index])
-            curr_list.pop()
-            dfs(index+1, curr_list, total)
-        dfs(index, curr_list, total)
+            #explore all candidates
+            for i in range(start, len(candidates)):
+                #check candidate validity
+                candidate.append(candidates[i])
+                #backtrack
+                total += candidates[i]
+                backtrack(candidate, i, total)
+                #remove
+                candidate.pop()
+                total -= candidates[i]
+                
+        backtrack([], 0, 0)
         return res
